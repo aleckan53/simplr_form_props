f44272896512b4c24d9bce6b98a48e175373c1fb
aleckan53 <admin@admins-MacBook-Pro-4.local>
2019-07-25 17:08:24 -0700

    pass useInput to Input as attribute

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import Input from './Input';
+import useInput from 'hooks/useInput';
 
 const Form = () => {
 
   const [state, setState] = useState({});
 
+  const { value: businessName, ...nameHandler } = useInput('Business Name', 'text');
+
   return (
     <form>
-      <Input
-        type='text'
-        label='Business Name'/>
+      <Input {...nameHandler}/>
       <Input
         type='submit'/>
     </form>
