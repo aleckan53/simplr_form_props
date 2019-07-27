bb85c4ba96b222b55dcaea7261fb756a7a967fc8
aleckan53 <admin@admins-MacBook-Pro-4.local>
2019-07-26 21:55:27 -0700

    CSS: improve layout responsivness

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -19,14 +19,14 @@ const Form = () => {
       <legend>LET'S GET STARTED</legend>
         <Input label='Business name' id='businessName' optional/>
         <Input label='Business email' id='email' type='email'/>
-        <Input label='Create a username' id='username'/>
+        <Input label='Create a Username' id='username'/>
         <Input label='Password' id='password' type='password' hint='6 characters | 1 uppercase | 1 lowercase | 1 digit'/>
         <Input label='Website' id='website' optional hint='(Optional)'/>
         <Select label='Type of Business' id='businessType' optional/>
-        <Input label='Terms of service' id='terms' type='checkbox'>
+        <Input label='Terms of Service' id='terms' type='checkbox'>
           <p className='checkBoxDescription'>I have read and I do accept <a href='www.google.com'>terms of services</a></p>
         </Input>
-        <Input label='Privacy policy' id='policy' type='checkbox'>
+        <Input label='Privacy Policy' id='policy' type='checkbox'>
           <p className='checkBoxDescription'>I have read and I do accept <a href='www.google.com'>privacy policy</a></p>
         </Input>
         <Button name='Register' type='submit' disabled={!allValid}/>
