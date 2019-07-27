deda4d6de5c5ff6ed93ddd32eafd35383c0a2fb8
aleckan53 <admin@admins-MacBook-Pro-5.local>
2019-07-27 15:15:40 -0700

    reorganize folders structure

--- src/__tests__/Select.test.js
+++ src/__tests__/Select.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import Select from '../Select';
+import Select from '../components/Select';
 
 describe('Select', () => {
   let wrapper;
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
-import App from './App/App';
-import Form from './Form/Form';
-import Input from './Input/Input';
-import Button from './Button/Button';
-import Select from './Select/Select';
+import App from './App';
+import Form from './Form';
+import Input from './Input';
+import Button from './Button';
+import Select from './Select';
 
 export {
   App, Form, Input, Button, Select,
